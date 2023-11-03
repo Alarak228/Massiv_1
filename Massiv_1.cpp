1651b94bd91f702f973926f29d083fc3cfa77925
Alarak228 <146061422+Alarak228@users.noreply.github.com>
2023-11-03 09:50:50 +0300

    Add files via upload

---
+++ Massiv_1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+const int SizeMassiv1 = 20;
+const int SizeMassiv2 = 10;
+int num1[SizeMassiv1];
+int num2[SizeMassiv2];
+void random();
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(0));
+    random();
+    return 0;
+}
+void random() 
+{
+    for (int i = 0; i < SizeMassiv1; i++) {
+        num1[i] = 1 + rand() % 20;
+        cout << num1[i] << "";
+
+    }
+    cout << endl;
+    for (int j = 0; j < SizeMassiv2; j++) {
+        num2[j] = 1 + rand() % 20;
+        cout << num2[j] << "";
+    }
+}
